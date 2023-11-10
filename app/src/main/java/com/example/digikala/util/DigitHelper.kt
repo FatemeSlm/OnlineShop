4623b2fa807778dba49b17da04c1d2803f1c120c
Fateme <salameh.f71@gmail.com>
2023-11-10 14:19:35 +0330

    add digit helper class

---
+++ app/src/main/java/com/example/digikala/util/DigitHelper.kt
@@ -0,0 +1,46 @@
+package com.example.digikala.util
+
+import java.text.DecimalFormat
+
+object DigitHelper {
+    fun digitByLocate(englishStr: String): String {
+        var result = ""
+        var fa = '۰'
+        for (ch in englishStr) {
+            when (ch) {
+                '0' -> fa = '۰'
+                '1' -> fa = '۱'
+                '2' -> fa = '۲'
+                '3' -> fa = '۳'
+                '4' -> fa = '۴'
+                '5' -> fa = '۵'
+                '6' -> fa = '۶'
+                '7' -> fa = '۷'
+                '8' -> fa = '۸'
+                '9' -> fa = '۹'
+            }
+            result = "$result $fa"
+        }
+        return result
+    }
+
+    fun digitBySeparator(price: String): String {
+        val priceFormat = DecimalFormat("###,###")
+        return priceFormat.format(Integer.valueOf(price))
+    }
+
+    fun digitByLocateAndSeparator(price: String): String {
+        val priceWithoutCommas = price.replace(",", "")
+        val persianDigit = digitByLocate(priceWithoutCommas)
+        return digitBySeparator(persianDigit)
+    }
+
+    fun applyDisCount(price: Long, discountPercent: Int): Long {
+        return if (discountPercent > 0) {
+            val discountAmount = price * discountPercent / 100
+            price - discountAmount
+        } else {
+            price
+        }
+    }
+}
